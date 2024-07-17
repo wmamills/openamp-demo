4f65313254a6de19906c6b06c27baa8dd33cb9e0
Bill Mills <bill.mills@linaro.org>
2024-07-16 23:19:08 -0400

    docker: split-user-dev adjust whitespace
    Now that it works, indent the new sub-functions.
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

--- docker/split-user-dev.sh
+++ docker/split-user-dev.sh
@@ -20,75 +20,75 @@ move-to() {
 }
 
 do_git_archives() {
-cd $SOURCE/..
-echo "make archive of user-dev template"
-(cd docker/user-dev; git archive -o $SOURCE/user-dev.tar.gz HEAD -- .)
+    cd $SOURCE/..
+    echo "make archive of user-dev template"
+    (cd docker/user-dev; git archive -o $SOURCE/user-dev.tar.gz HEAD -- .)
 
-echo "make archive of qemu-zcu102"
-git archive -o $SOURCE/qemu-zcu102.tar.gz HEAD -- qemu-zcu102
+    echo "make archive of qemu-zcu102"
+    git archive -o $SOURCE/qemu-zcu102.tar.gz HEAD -- qemu-zcu102
 
-echo "make archive of demos"
-(cd demos; git archive -o $SOURCE/demos.tar.gz HEAD -- *)
+    echo "make archive of demos"
+    (cd demos; git archive -o $SOURCE/demos.tar.gz HEAD -- *)
 }
 
 user_dev_inner() {
-cd $ORIG
-rm -rf xxx-temp-base|| true
-mkdir xxx-temp-base
-rm -rf xxx-temp-extra || true
-mkdir xxx-temp-extra
-rm -rf xxx-temp-trash || true
-mkdir xxx-temp-trash
-rm -rf xxx-temp-out || true
-mkdir xxx-temp-out
-OUT=$ORIG/xxx-temp-out
+    cd $ORIG
+    rm -rf xxx-temp-base|| true
+    mkdir xxx-temp-base
+    rm -rf xxx-temp-extra || true
+    mkdir xxx-temp-extra
+    rm -rf xxx-temp-trash || true
+    mkdir xxx-temp-trash
+    rm -rf xxx-temp-out || true
+    mkdir xxx-temp-out
+    OUT=$ORIG/xxx-temp-out
 
-cd $ORIG/xxx-temp-base
-TOP=$PWD
-mkdir -p opt
+    cd $ORIG/xxx-temp-base
+    TOP=$PWD
+    mkdir -p opt
 
-echo "copy user-dev template"
-tar xvf $SOURCE/user-dev.tar.gz
+    echo "copy user-dev template"
+    tar xvf $SOURCE/user-dev.tar.gz
 
-echo "copy in the demos directory"
-tar xvf $SOURCE/demos.tar.gz
+    echo "copy in the demos directory"
+    tar xvf $SOURCE/demos.tar.gz
 
-echo "copy in the qemu-zcu102 files"
-tar xvf $SOURCE/qemu-zcu102.tar.gz -C opt
+    echo "copy in the qemu-zcu102 files"
+    tar xvf $SOURCE/qemu-zcu102.tar.gz -C opt
 
-echo "copy in the pre-installed image of the zephyr-sdk"
-tar xvf $SOURCE/zephyr-sdk-0.15.1-installed.tar.gz -C opt
+    echo "copy in the pre-installed image of the zephyr-sdk"
+    tar xvf $SOURCE/zephyr-sdk-0.15.1-installed.tar.gz -C opt
 
-echo "fixup the symlinks"
-cd opt
-ln -sf ../../../../zephyr-sdk-0.15.1/sysroots/x86_64-pokysdk-linux/usr/xilinx/bin/qemu-system-aarch64 \
-    ./qemu-zcu102/sysroot/usr/bin/qemu-system-aarch64
-ln -sf ../../../../zephyr-sdk-0.15.1/sysroots/x86_64-pokysdk-linux/usr/xilinx/bin/qemu-system-microblazeel \
-    ./qemu-zcu102/sysroot/usr/bin/qemu-system-microblazeel
+    echo "fixup the symlinks"
+    cd opt
+    ln -sf ../../../../zephyr-sdk-0.15.1/sysroots/x86_64-pokysdk-linux/usr/xilinx/bin/qemu-system-aarch64 \
+        ./qemu-zcu102/sysroot/usr/bin/qemu-system-aarch64
+    ln -sf ../../../../zephyr-sdk-0.15.1/sysroots/x86_64-pokysdk-linux/usr/xilinx/bin/qemu-system-microblazeel \
+        ./qemu-zcu102/sysroot/usr/bin/qemu-system-microblazeel
 
-echo "Start the splitting operation"
-cd $TOP/opt/zephyr-sdk-0.15.1
-move-to extra arm-zephyr-eabi
-move-to extra aarch64-zephyr-elf
-cd sysroots/x86_64-pokysdk-linux/usr
-move-to extra bin/qemu-*
-move-to extra bin/openocd
-move-to extra synopsys
+    echo "Start the splitting operation"
+    cd $TOP/opt/zephyr-sdk-0.15.1
+    move-to extra arm-zephyr-eabi
+    move-to extra aarch64-zephyr-elf
+    cd sysroots/x86_64-pokysdk-linux/usr
+    move-to extra bin/qemu-*
+    move-to extra bin/openocd
+    move-to extra synopsys
 
-cd $TOP
+    cd $TOP
 
-move-to extra test-sw/openamp-ci-*/modules-*.tgz
+    move-to extra test-sw/openamp-ci-*/modules-*.tgz
 
-tar czvf $OUT/user-dev-base.tar.gz .
-cd $ORIG/xxx-temp-extra
-tar czvf $OUT/user-dev-extra.tar.gz .
-cd $ORIG/xxx-temp-trash
-tar czvf $OUT/user-dev-trash.tar.gz .
+    tar czvf $OUT/user-dev-base.tar.gz .
+    cd $ORIG/xxx-temp-extra
+    tar czvf $OUT/user-dev-extra.tar.gz .
+    cd $ORIG/xxx-temp-trash
+    tar czvf $OUT/user-dev-trash.tar.gz .
 
-cd $ORIG
-rm -rf $ORIG/xxx-temp-base
-rm -rf $ORIG/xxx-temp-extra
-rm -rf $ORIG/xxx-temp-trash
+    cd $ORIG
+    rm -rf $ORIG/xxx-temp-base
+    rm -rf $ORIG/xxx-temp-extra
+    rm -rf $ORIG/xxx-temp-trash
 }
 
 do_user_dev() {
