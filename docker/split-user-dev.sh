47c0ee728300bd168222f88aa71741e20192c50c
Bill Mills <bill.mills@linaro.org>
2024-07-16 22:48:49 -0400

    docker: adjust split-user-dev to work inside a container
    Building the user-dev-* tarfiles inside a container allows more isolation,
    and UID:GID independence.
    
    * break out git_archive and user_dev into sub functions
    * create the git archives at the host level
    * split ORIG and SOURCE so inside container ORIG can be /home/dev and
      SOURCE can be /prj/docker when running in the container
    * allow user_dev to be run at host level and in container (for now)
    * add the shell mechanics to recurse into different contexts
    * create the user-dev-* tarfiles in a temporary dir before moving them
      to the correct place in the outer function
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

--- docker/split-user-dev.sh
+++ docker/split-user-dev.sh
@@ -19,42 +19,48 @@ move-to() {
     done
 }
 
-ORIG=$PWD
+do_git_archives() {
+cd $SOURCE/..
+echo "make archive of user-dev template"
+(cd docker/user-dev; git archive -o $SOURCE/user-dev.tar.gz HEAD -- .)
+
+echo "make archive of qemu-zcu102"
+git archive -o $SOURCE/qemu-zcu102.tar.gz HEAD -- qemu-zcu102
+
+echo "make archive of demos"
+(cd demos; git archive -o $SOURCE/demos.tar.gz HEAD -- *)
+}
+
+user_dev_inner() {
+cd $ORIG
 rm -rf xxx-temp-base|| true
 mkdir xxx-temp-base
 rm -rf xxx-temp-extra || true
 mkdir xxx-temp-extra
 rm -rf xxx-temp-trash || true
 mkdir xxx-temp-trash
-
-cd ..
-echo "make archive of user-dev template"
-(cd docker/user-dev; git archive -o $ORIG/user-dev.tar.gz HEAD -- .)
-
-echo "make archive of qemu-zcu102"
-git archive -o $ORIG/qemu-zcu102.tar.gz HEAD -- qemu-zcu102
-
-echo "make archive of demos"
-(cd demos; git archive -o $ORIG/demos.tar.gz HEAD -- *)
+rm -rf xxx-temp-out || true
+mkdir xxx-temp-out
+OUT=$ORIG/xxx-temp-out
 
 cd $ORIG/xxx-temp-base
 TOP=$PWD
 mkdir -p opt
 
 echo "copy user-dev template"
-tar xvf $ORIG/user-dev.tar.gz
+tar xvf $SOURCE/user-dev.tar.gz
 
 echo "copy in the demos directory"
-tar xvf $ORIG/demos.tar.gz
+tar xvf $SOURCE/demos.tar.gz
 
 echo "copy in the qemu-zcu102 files"
-tar xvf $ORIG/qemu-zcu102.tar.gz -C opt
+tar xvf $SOURCE/qemu-zcu102.tar.gz -C opt
 
 echo "copy in the pre-installed image of the zephyr-sdk"
-tar xvf $ORIG/zephyr-sdk-0.15.1-installed.tar.gz -C opt
+tar xvf $SOURCE/zephyr-sdk-0.15.1-installed.tar.gz -C opt
 
 echo "fixup the symlinks"
-cd $TOP/opt
+cd opt
 ln -sf ../../../../zephyr-sdk-0.15.1/sysroots/x86_64-pokysdk-linux/usr/xilinx/bin/qemu-system-aarch64 \
     ./qemu-zcu102/sysroot/usr/bin/qemu-system-aarch64
 ln -sf ../../../../zephyr-sdk-0.15.1/sysroots/x86_64-pokysdk-linux/usr/xilinx/bin/qemu-system-microblazeel \
@@ -73,13 +79,68 @@ cd $TOP
 
 move-to extra test-sw/openamp-ci-*/modules-*.tgz
 
-tar czvf $ORIG/demo-lite/user-dev-base.tar.gz .
+tar czvf $OUT/user-dev-base.tar.gz .
 cd $ORIG/xxx-temp-extra
-tar czvf $ORIG/demo/user-dev-extra.tar.gz .
+tar czvf $OUT/user-dev-extra.tar.gz .
 cd $ORIG/xxx-temp-trash
-tar czvf $ORIG/user-dev-trash.tar.gz .
+tar czvf $OUT/user-dev-trash.tar.gz .
 
 cd $ORIG
 rm -rf $ORIG/xxx-temp-base
 rm -rf $ORIG/xxx-temp-extra
 rm -rf $ORIG/xxx-temp-trash
+}
+
+do_user_dev() {
+    user_dev_inner
+    user_dev_outer
+}
+
+# we are inside the container but we are root
+user_dev_outer() {
+    OUT=$ORIG/xxx-temp-out
+
+    # now copy the archives to the correct place
+    HOST_UID=$(stat -c %u $ME)
+    HOST_GID=$(stat -c %g $ME)
+    chown $HOST_UID:$HOST_GID $OUT/user-dev-*.tar.gz
+    cp -p $OUT/user-dev-base.tar.gz $SOURCE/demo-lite/.
+    cp -p $OUT/user-dev-extra.tar.gz $SOURCE/demo/.
+    cp -p $OUT/user-dev-trash.tar.gz $SOURCE/.
+
+    rm -rf $ORIG/xxx-temp-out
+}
+
+do_sudo_user_dev() {
+    ORIG=$HOME
+    SOURCE=/prj/docker
+    DEST=$HOME
+    user_dev_inner
+}
+
+# we are inside the container but we are root
+do_inside_container() {
+    # do the bulk of the work as the "dev" user
+    sudo -Hu dev $ME sudo_user_dev
+
+    ORIG=/home/dev
+    SOURCE=/prj/docker
+    DEST=$ORIG
+    user_dev_outer
+}
+
+ME=$0
+cd $(dirname $0)
+ORIG=$PWD
+SOURCE=$PWD
+CMD=$1
+case $CMD in
+"user_dev"|"git_archives"|"inside_container"|"sudo_user_dev")
+    shift
+    do_$CMD
+    ;;
+*)
+    echo "Unknown sub-command $1"
+    exit 2
+    ;;
+esac
